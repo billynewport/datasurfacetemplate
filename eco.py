56c04640cbeeb163146fa74924deccd3bc67e7e7
Billy Newport <billy@billynewport.com>
2024-02-28 07:43:35 -0500

    Updating workflow file

--- eco.py
+++ eco.py
@@ -1,2 +1,6 @@
-import Ecosystem
+from datasurface.md.Governance import Ecosystem
 
+
+def createEcosystem() -> Ecosystem:
+    return Ecosystem("test", GithubRepository("test", "owner", "repo")
+                     )
