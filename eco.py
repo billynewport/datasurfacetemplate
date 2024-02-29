3eb10ecc6270a7b40016edeeb80af564f1e114a3
Billy Newport <billy@billynewport.com>
2024-02-29 14:35:40 -0500

    Step

--- eco.py
+++ eco.py
@@ -1,7 +1,63 @@
-from datasurface.md.Governance import Ecosystem
+from datasurface.md.AmazonAWS import AmazonAWSDataPlatform
+from datasurface.md.Azure import AzureDataplatform, AzureKeyVaultCredential
+from datasurface.md.Documentation import PlainTextDocumentation
 from datasurface.md.GitOps import GitHubRepository
+from datasurface.md.Governance import CloudVendor, DefaultDataPlatform, Ecosystem, GovernanceZoneDeclaration, InfrastructureLocation, InfrastructureVendor
+
+# Base branch for step 1, define an Ecosystem, data platforms, infrastructure vendors/locations and 3 Governance Zones
 
 
 def createEcosystem() -> Ecosystem:
-    return Ecosystem("Test", GitHubRepository("billynewport/datasurfacetemplate", "main")
-                     )
+    e: Ecosystem = Ecosystem(
+        "Test", GitHubRepository("billynewport/datasurfacetemplate", "main"),
+        DefaultDataPlatform(AzureDataplatform("Azure Platform", PlainTextDocumentation("Test"),
+                            AzureKeyVaultCredential("vault", "maincred"))),
+        AmazonAWSDataPlatform("AWS Platform", PlainTextDocumentation("Test")),
+
+        GovernanceZoneDeclaration("USA", GitHubRepository("billynewport/datasurfacetemplate", "USAmain")),
+        GovernanceZoneDeclaration("EU", GitHubRepository("billynewport/datasurfacetemplate", "EUmain")),
+        GovernanceZoneDeclaration("UK", GitHubRepository("billynewport/datasurfacetemplate", "UKmain")),
+
+        # Infra Vendors and locations
+        InfrastructureVendor(
+            "AWS",
+            CloudVendor.AWS,
+            PlainTextDocumentation("Amazon AWS"),
+            InfrastructureLocation(
+                "USA",
+                InfrastructureLocation("us-east-1"),  # Virginia
+                InfrastructureLocation("us-west-1")),
+            InfrastructureLocation(
+                "UK",
+                InfrastructureLocation("eu-west-1"),  # Ireland
+                InfrastructureLocation("eu-west-2")),  # London
+            InfrastructureLocation(
+                "EU",
+                InfrastructureLocation("eu-central-1"),  # Frankfurt
+                InfrastructureLocation("eu-west-3"))),
+
+        InfrastructureVendor(
+            "Azure",
+            CloudVendor.AZURE,
+            PlainTextDocumentation("Microsoft Azure"),
+            InfrastructureLocation(
+                "USA",
+                InfrastructureLocation(
+                    "Central",
+                    InfrastructureLocation("Central US"),  # Iowa
+                    InfrastructureLocation("North Central US"),  # Illinois
+                    InfrastructureLocation("South Central US"),  # Texas
+                    InfrastructureLocation("West Central US")),  # Wyoming
+                InfrastructureLocation(
+                    "East",
+                    InfrastructureLocation("East US"),  # Virginia
+                    InfrastructureLocation("East US 2"),  # Virginia
+                    InfrastructureLocation("East US 3")),  # Georgia
+                InfrastructureLocation(
+                    "West",
+                    InfrastructureLocation("West US"),  # California
+                    InfrastructureLocation("West US 2"),  # Washington
+                    InfrastructureLocation("West US 3"))))  # Arizona
+        )
+    return e
+
